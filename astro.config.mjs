93ee0c21cd6a132db0edf90ecc56688e4d39eca3
Pavel Maksimovich <63461675+Fox1N69@users.noreply.github.com>
2024-06-06 02:14:31 +0500

    setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), compress()]
+  image: {
+    service: passthroughImageService(),
+  },
+  integrations: [tailwind(), react(), compress()],
 });
