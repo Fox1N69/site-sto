bdf118d08da3f6ca8fcccd5ff4ceb6c2a740a435
Pavel Maksimovich <63461675+Fox1N69@users.noreply.github.com>
2024-06-10 16:56:29 +0500

    Реализовал интерфейс репозитория корзины

--- landing/server/internal/repo/basket_repo.go
+++ landing/server/internal/repo/basket_repo.go
@@ -7,8 +7,10 @@ import (
 )
 
 type BasketRepo interface {
-	Create(basket model.Basket) error
-	AddItems(baskte model.Basket, items []model.BasketItem) error
+	Create(basket *model.Basket) error
+	GetAllItems(basket *model.Basket) ([]model.BasketItem, error)
+	AddItem(basket *model.Basket, items []model.BasketItem) error
+	RemoveItem(basket *model.Basket, items []model.BasketItem) error
 }
 
 type basketRepo struct {
@@ -19,10 +21,18 @@ func NewBasketRepo(db *gorm.DB) BasketRepo {
 	return &basketRepo{db: db}
 }
 
-func (r *basketRepo) Create(basket model.Basket) error {
+func (r *basketRepo) Create(basket *model.Basket) error {
 	return r.db.Create(basket).Error
 }
 
-func (r *basketRepo) AddItems(basket model.Basket, items []model.BasketItem) error {
+func (r *basketRepo) GetAllItems(basket *model.Basket) ([]model.BasketItem, error) {
+	return nil, nil
+}
+
+func (r *basketRepo) AddItem(basket *model.Basket, items []model.BasketItem) error {
+	return nil
+}
+
+func (r *basketRepo) RemoveItem(basket *model.Basket, items []model.BasketItem) error {
 	return nil
 }
