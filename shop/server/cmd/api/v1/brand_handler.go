463267d54cae9a14aadb656b643d6da457e66049
Pavel Maksimovich <63461675+Fox1N69@users.noreply.github.com>
2024-06-11 20:43:11 +0500

    create brand handler

---
+++ shop/server/cmd/api/v1/brand_handler.go
@@ -0,0 +1,33 @@
+package v1
+
+import (
+	"net/http"
+	"shop-server/common/http/response"
+	"shop-server/internal/model"
+	"shop-server/internal/service"
+
+	"github.com/gin-gonic/gin"
+)
+
+type BrandHandler interface {
+}
+
+type brandHandler struct {
+	service service.BrandService
+}
+
+func NewBrandHandler(brandService service.BrandService) BrandHandler {
+	return &brandHandler{service: brandService}
+}
+
+func (h *brandHandler) CreateBrand(c *gin.Context) {
+	data := new(model.Brand)
+	c.BindJSON(data)
+
+	if err := h.service.CreateBrand(data); err != nil {
+		response.New(c).Write(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, "brand create success")
+}
