6bbf60d0dce413007f278843a0e2632a6f766117
Pavel Maksimovich <63461675+Fox1N69@users.noreply.github.com>
2024-06-11 20:22:43 +0500

    реализовал brand service

---
+++ shop/server/internal/service/brand_service.go
@@ -0,0 +1,45 @@
+package service
+
+import (
+	"shop-server/internal/model"
+	"shop-server/internal/repo"
+)
+
+type BrendService interface {
+	CreateBrand(brand *model.Brand) error
+	GetBrandByID(id uint) (*model.Brand, error)
+	UpdateBrand(brand *model.Brand) error
+	DeleteBrand(brandID uint) error
+	AssociateCategoryFromBrand(brandID uint, categoryID uint) error
+	RemoveCategoryFromBrand(brandID uint, categoryID uint) error
+}
+
+type brandService struct {
+	brandRepo repo.BrandRepo
+}
+
+func NewBrandService(brandRepo repo.BrandRepo) BrendService {
+	return &brandService{brandRepo: brandRepo}
+}
+
+func (s *brandService) CreateBrand(brand *model.Brand) error {
+	return s.brandRepo.CreateBrand(brand)
+}
+
+func (s *brandService) GetBrandByID(id uint) (*model.Brand, error) {
+	return s.brandRepo.GetBrandByID(id)
+}
+
+func (s *brandService) UpdateBrand(brand *model.Brand) error {
+	return s.brandRepo.UpdateBrand(brand)
+}
+
+func (s *brandService) DeleteBrand(brandID uint) error {
+	return s.brandRepo.DeleteBrand(brandID)
+}
+func (s *brandService) AssociateCategoryFromBrand(brandID uint, categoryID uint) error {
+	return s.brandRepo.AssociateCategoryFromBrand(brandID, categoryID)
+}
+func (s *brandService) RemoveCategoryFromBrand(brandID uint, categoryID uint) error {
+	return s.brandRepo.RemoveCategoryFromBrand(brandID, categoryID)
+}
