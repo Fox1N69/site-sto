b3fc32d1301cf05c50edee1864d1d4ace92e61f0
Pavel Maksimovich <63461675+Fox1N69@users.noreply.github.com>
2024-06-12 08:13:06 +0500

    work tsx

--- autopart-shop/app/page.tsx
+++ autopart-shop/app/page.tsx
@@ -31,7 +31,6 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24 gap-20">
-      <Navbar />
       <div className="main__container">
         <Banner />
         <div className="autopart__cards flex gap-5 justify-center mt-20">
