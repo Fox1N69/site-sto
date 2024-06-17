133f6861432c4b298487d03dfd016f7c883b3e1b
Pavel Maksimovich <63461675+Fox1N69@users.noreply.github.com>
2024-06-18 00:19:14 +0500

    Настроил компонент карточки брендов

--- autopart-shop/components/home/BrandCard.tsx
+++ autopart-shop/components/home/BrandCard.tsx
@@ -1,23 +1,26 @@
+import { Brand } from '@/types';
 import { Card, CardBody, CardHeader } from '@nextui-org/react';
 import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 interface BrandCardProps {
-	id: number;
-	name: string;
-	img_url: string;
+	brand: Brand;
 }
 
-const BrandCard: React.FC<BrandCardProps> = ({ id, name, img_url }) => {
+const BrandCard: React.FC<BrandCardProps> = ({ brand }) => {
 	return (
 		<Card style={{ width: 100, height: 100 }}>
-			<Image
-				alt={''}
-				className='object-cover'
-				height={200}
-				src={img_url}
-				width={200}
-			/>
+			<Link href={`/brand/${brand.id}`}>
+				<Image
+					alt={''}
+					className='object-cover'
+					height={200}
+					src={brand.img_url}
+					width={200}
+				/>
+			</Link>
 		</Card>
 	);
 };
--- autopart-shop/types.d.ts
+++ autopart-shop/types.d.ts
@@ -1,48 +1,49 @@
 export interface AutoPart {
-  id: number;
-  name: string;
-  price: number;
-  img: string;
-  category_id: number;
-  brand_id: number;
-  stock: number;
-  auto_part_info?: AutoPartInfo[];
-  Category?: Category;
-  Brand?: Brand;
-  model_name?: string;
+	id: number;
+	name: string;
+	price: number;
+	img: string;
+	category_id: number;
+	brand_id: number;
+	stock: number;
+	auto_part_info?: AutoPartInfo[];
+	Category?: Category;
+	Brand?: Brand;
+	model_name?: string;
 }
 
 interface Product {
-  id: number;
-  name: string;
-  href: string;
-  color: string;
-  price: number;
-  quantity: number;
-  imageSrc: string;
-  imageAlt: string;
-  cartItemID: number;
+	id: number;
+	name: string;
+	href: string;
+	color: string;
+	price: number;
+	quantity: number;
+	imageSrc: string;
+	imageAlt: string;
+	cartItemID: number;
 }
 
 export interface AutoPartInfo {
-  id: number;
-  title: string;
-  description: string;
+	id: number;
+	title: string;
+	description: string;
 }
 
 export interface Category {
-  id: number;
-  name: string;
+	id: number;
+	name: string;
 }
 
 export interface Brand {
-  id: number;
-  name: string;
+	id: number;
+	name: string;
+	img_url: string;
 }
 
 export interface User {
-  id: number;
-  fio?: string;
-  username?: string;
-  role?: string;
+	id: number;
+	fio?: string;
+	username?: string;
+	role?: string;
 }
