b2a1c44a438210bd98fcb19bb9b98bb07f1e0fca
Pavel Maksimovich <63461675+Fox1N69@users.noreply.github.com>
2024-06-17 05:28:40 +0500

    Настроил pritter

--- autopart-shop/app/page.tsx
+++ autopart-shop/app/page.tsx
@@ -1,37 +1,37 @@
-"use client";
+'use client';
 
-import Banner from "@/components/Banner/banner";
-import Card from "@/components/product/ProductCard/Card";
-import { AutoPart } from "@/types";
-import axios from "axios";
-import Image from "next/image";
-import { useEffect, useState } from "react";
-import { Navbar } from "@nextui-org/navbar";
-import { AuthProvider } from "@/components/context/authContext";
-import { useRouter } from "next/navigation";
-import { useFetchAutoParts } from "@/hooks/fetching";
-import Example from "@/components/cart/CartModal";
-import { Button } from "@nextui-org/button";
-import { PartCard } from "@/components/product/ProductCard/PartCard";
-import { CategoryCard } from "@/components/home/CategoryCard";
+import Banner from '@/components/Banner/banner';
+import Card from '@/components/product/ProductCard/Card';
+import { AutoPart } from '@/types';
+import axios from 'axios';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import { Navbar } from '@nextui-org/navbar';
+import { AuthProvider } from '@/components/context/authContext';
+import { useRouter } from 'next/navigation';
+import { useFetchAutoParts } from '@/hooks/fetching';
+import Example from '@/components/cart/CartModal';
+import { Button } from '@nextui-org/button';
+import { PartCard } from '@/components/product/ProductCard/PartCard';
+import { CategoryCard } from '@/components/home/CategoryCard';
 
 export default function Home() {
-  const router = useRouter();
-  const autoParts = useFetchAutoParts();
+	const router = useRouter();
+	const autoParts = useFetchAutoParts();
 
-  return (
-    <main className="flex min-h-screen flex-col items-center p-24 gap-20">
-      <div className="main__container">
-        <Banner />
-        <div className="category__cards w-[200px]">
-          <CategoryCard key={1} id={1} name="Двигатель" />
-        </div>
-        <div className="autopart__cards flex gap-5 justify-center mt-20">
-          {autoParts.slice(0, 5).map((part) => (
-            <PartCard key={part.id} part={part} />
-          ))}
-        </div>
-      </div>
-    </main>
-  );
+	return (
+		<main className='flex min-h-screen flex-col items-center p-24 gap-20'>
+			<div className='main__container'>
+				<Banner />
+				<div className='category__cards w-[200px]'>
+					<CategoryCard key={1} id={1} name='Двигатель' />
+				</div>
+				<div className='autopart__cards flex gap-5 justify-center mt-20'>
+					{autoParts.slice(0, 5).map(part => (
+						<PartCard key={part.id} part={part} />
+					))}
+				</div>
+			</div>
+		</main>
+	);
 }
--- autopart-shop/components/filters/index.tsx
+++ autopart-shop/components/filters/index.tsx
@@ -1,29 +1,26 @@
-import { filterProps } from "framer-motion";
-import PriceFilter from "./filter-price";
-import { Button } from "@nextui-org/button";
-import { Card } from "@nextui-org/react";
-import BrandFileter from "./brand-filter";
+import { filterProps } from 'framer-motion';
+import PriceFilter from './filter-price';
+import { Button } from '@nextui-org/button';
+import { Card } from '@nextui-org/react';
 
 const Filters = () => {
-  const brand_name = "test";
-  const category = "test";
-  return (
-    <Card className=" rounded-lg">
-      <PriceFilter
-        onFilter={() => {
-          console.log("price - filter");
-        }}
-      />
+	return (
+		<Card className='rounded-lg'>
+			<PriceFilter
+				onFilter={() => {
+					console.log('price - filter');
+				}}
+			/>
 
-      <Button
-        onPress={() => {
-          console.log("фильтры применен");
-        }}
-      >
-        Применить фильтр
-      </Button>
-    </Card>
-  );
+			<Button
+				onPress={() => {
+					console.log('фильтры применен');
+				}}
+			>
+				Применить фильтр
+			</Button>
+		</Card>
+	);
 };
 
 export default Filters;
