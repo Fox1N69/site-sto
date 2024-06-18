2bc11ed1cef73238175c8009cd55e9414adc702e
Pavel Maksimovich <63461675+Fox1N69@users.noreply.github.com>
2024-06-19 01:23:08 +0500

    Изменил название магазина в navbar

--- autopart-shop/components/navbar/navbar.tsx
+++ autopart-shop/components/navbar/navbar.tsx
@@ -1,185 +1,185 @@
 // components/navbar/navbar.tsx
-"use client";
+'use client';
 import {
-  Navbar as NextUINavbar,
-  NavbarContent,
-  NavbarMenu,
-  NavbarMenuToggle,
-  NavbarBrand,
-  NavbarItem,
-  NavbarMenuItem,
-} from "@nextui-org/navbar";
-import { Button } from "@nextui-org/button";
-import { Kbd } from "@nextui-org/kbd";
-import { Link } from "@nextui-org/link";
-import { Input } from "@nextui-org/input";
-import { link as linkStyles } from "@nextui-org/theme";
-import NextLink from "next/link";
-import clsx from "clsx";
+	Navbar as NextUINavbar,
+	NavbarContent,
+	NavbarMenu,
+	NavbarMenuToggle,
+	NavbarBrand,
+	NavbarItem,
+	NavbarMenuItem
+} from '@nextui-org/navbar';
+import { Button } from '@nextui-org/button';
+import { Kbd } from '@nextui-org/kbd';
+import { Link } from '@nextui-org/link';
+import { Input } from '@nextui-org/input';
+import { link as linkStyles } from '@nextui-org/theme';
+import NextLink from 'next/link';
+import clsx from 'clsx';
 
-import { siteConfig } from "@/config/site";
-import { ThemeSwitch } from "@/components/theme-switch";
-import { GithubIcon, SearchIcon, Logo } from "@/components/icons";
-import { useState, useRef, useEffect } from "react";
-import { UserDropdown } from "./user-dropdown";
-import { useSession } from "next-auth/react";
-import axios from "axios";
-import Loaded from "@icons/lottie/loaded.json";
-import Lottie from "lottie-react";
-import CartModal from "../cart/CartModal";
-import { AutoPart } from "@/types";
+import { siteConfig } from '@/config/site';
+import { ThemeSwitch } from '@/components/theme-switch';
+import { GithubIcon, SearchIcon, Logo } from '@/components/icons';
+import { useState, useRef, useEffect } from 'react';
+import { UserDropdown } from './user-dropdown';
+import { useSession } from 'next-auth/react';
+import axios from 'axios';
+import Loaded from '@icons/lottie/loaded.json';
+import Lottie from 'lottie-react';
+import CartModal from '../cart/CartModal';
+import { AutoPart } from '@/types';
 
 export const Navbar = () => {
-  const { data: session, status } = useSession();
-  const [searchQuery, setSearchQuery] = useState("");
-  const [searchResults, setSearchResults] = useState<AutoPart[]>([]);
-  const [showDropdown, setShowDropdown] = useState(false);
-  const dropdownRef = useRef<HTMLDivElement>(null);
+	const { data: session, status } = useSession();
+	const [searchQuery, setSearchQuery] = useState('');
+	const [searchResults, setSearchResults] = useState<AutoPart[]>([]);
+	const [showDropdown, setShowDropdown] = useState(false);
+	const dropdownRef = useRef<HTMLDivElement>(null);
 
-  const handleSearchInputChange = async (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    const query = event.target.value;
-    setSearchQuery(query);
-    if (query.length > 1) {
-      try {
-        const response = await axios.get(
-          `http://localhost:4000/shop/autoparts/search?query=${query}`
-        );
-        setSearchResults(response.data);
-        setShowDropdown(true);
-      } catch (error) {
-        console.error("Failed to fetch search results", error);
-        setSearchResults([]);
-        setShowDropdown(false);
-      }
-    } else {
-      setSearchResults([]);
-      setShowDropdown(false);
-    }
-  };
+	const handleSearchInputChange = async (
+		event: React.ChangeEvent<HTMLInputElement>
+	) => {
+		const query = event.target.value;
+		setSearchQuery(query);
+		if (query.length > 1) {
+			try {
+				const response = await axios.get(
+					`http://localhost:4000/shop/autoparts/search?query=${query}`
+				);
+				setSearchResults(response.data);
+				setShowDropdown(true);
+			} catch (error) {
+				console.error('Failed to fetch search results', error);
+				setSearchResults([]);
+				setShowDropdown(false);
+			}
+		} else {
+			setSearchResults([]);
+			setShowDropdown(false);
+		}
+	};
 
-  const handleClickOutside = (event: MouseEvent) => {
-    if (
-      dropdownRef.current &&
-      !dropdownRef.current.contains(event.target as Node)
-    ) {
-      setShowDropdown(false);
-    }
-  };
+	const handleClickOutside = (event: MouseEvent) => {
+		if (
+			dropdownRef.current &&
+			!dropdownRef.current.contains(event.target as Node)
+		) {
+			setShowDropdown(false);
+		}
+	};
 
-  useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
+	useEffect(() => {
+		document.addEventListener('mousedown', handleClickOutside);
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, []);
 
-  const searchInput = (
-    <div style={{ position: "relative" }}>
-      <Input
-        aria-label="Search"
-        classNames={{
-          inputWrapper: "bg-default-100",
-          input: "text-sm",
-        }}
-        onChange={handleSearchInputChange}
-        value={searchQuery}
-        endContent={
-          <Kbd className="hidden lg:inline-block" keys={["command"]}>
-            K
-          </Kbd>
-        }
-        labelPlacement="outside"
-        placeholder="Search..."
-        startContent={
-          <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-        }
-        type="search"
-      />
-      {showDropdown && searchResults.length > 0 && (
-        <div
-          ref={dropdownRef}
-          className="absolute z-10 w-full bg-white border border-gray-300 rounded-md shadow-lg mt-1"
-        >
-          {searchResults.map((result) => (
-            <div
-              key={result.id}
-              className="p-2 hover:bg-gray-100 cursor-pointer"
-            >
-              <Link href={`/autopart/${result.id}`}>
-                {result.name ||
-                  result.model_name ||
-                  result.Category?.name ||
-                  result.Brand?.name}
-              </Link>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+	const searchInput = (
+		<div style={{ position: 'relative' }}>
+			<Input
+				aria-label='Search'
+				classNames={{
+					inputWrapper: 'bg-default-100',
+					input: 'text-sm'
+				}}
+				onChange={handleSearchInputChange}
+				value={searchQuery}
+				endContent={
+					<Kbd className='hidden lg:inline-block' keys={['command']}>
+						K
+					</Kbd>
+				}
+				labelPlacement='outside'
+				placeholder='Search...'
+				startContent={
+					<SearchIcon className='text-base text-default-400 pointer-events-none flex-shrink-0' />
+				}
+				type='search'
+			/>
+			{showDropdown && searchResults.length > 0 && (
+				<div
+					ref={dropdownRef}
+					className='absolute z-10 w-full bg-white border border-gray-300 rounded-md shadow-lg mt-1'
+				>
+					{searchResults.map(result => (
+						<div
+							key={result.id}
+							className='p-2 hover:bg-gray-100 cursor-pointer'
+						>
+							<Link href={`/autopart/${result.id}`}>
+								{result.name ||
+									result.model_name ||
+									result.Category?.name ||
+									result.Brand?.name}
+							</Link>
+						</div>
+					))}
+				</div>
+			)}
+		</div>
+	);
 
-  return (
-    <NextUINavbar maxWidth="xl" position="sticky">
-      <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
-        <NavbarBrand as="li" className="gap-3 max-w-fit">
-          <NextLink className="flex justify-start items-center gap-1" href="/">
-            <Logo />
-            <p className="font-bold text-inherit">ACME</p>
-          </NextLink>
-        </NavbarBrand>
-        <ul className="hidden lg:flex gap-4 justify-start ml-2">
-          {siteConfig.navItems.map((item) => (
-            <NavbarItem key={item.href}>
-              <NextLink
-                className={clsx(
-                  linkStyles({ color: "foreground" }),
-                  "data-[active=true]:text-primary data-[active=true]:font-medium"
-                )}
-                color="foreground"
-                href={item.href}
-              >
-                {item.label}
-              </NextLink>
-            </NavbarItem>
-          ))}
-        </ul>
-      </NavbarContent>
+	return (
+		<NextUINavbar maxWidth='xl' position='sticky'>
+			<NavbarContent className='basis-1/5 sm:basis-full' justify='start'>
+				<NavbarBrand as='li' className='gap-3 max-w-fit'>
+					<NextLink className='flex justify-start items-center gap-1' href='/'>
+						<Logo />
+						<p className='font-bold text-inherit'>RemZona</p>
+					</NextLink>
+				</NavbarBrand>
+				<ul className='hidden lg:flex gap-4 justify-start ml-2'>
+					{siteConfig.navItems.map(item => (
+						<NavbarItem key={item.href}>
+							<NextLink
+								className={clsx(
+									linkStyles({ color: 'foreground' }),
+									'data-[active=true]:text-primary data-[active=true]:font-medium'
+								)}
+								color='foreground'
+								href={item.href}
+							>
+								{item.label}
+							</NextLink>
+						</NavbarItem>
+					))}
+				</ul>
+			</NavbarContent>
 
-      <NavbarContent
-        className="hidden sm:flex basis-1/5 sm:basis-full"
-        justify="end"
-      >
-        <NavbarItem className="hidden sm:flex gap-2">
-          <ThemeSwitch />
-        </NavbarItem>
-        <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
-        <NavbarItem className="hidden md:flex">
-          {status === "loading" ? (
-            <div className=" w-[75px] h-[75px]">
-              <Lottie animationData={Loaded} />
-            </div>
-          ) : session ? (
-            <div className="flex gap-3  items-center">
-              <CartModal />
-              <UserDropdown user={session?.user.id} />
-            </div>
-          ) : (
-            <Button as={NextLink} href="/auth/login">
-              Login
-            </Button>
-          )}
-        </NavbarItem>
-      </NavbarContent>
+			<NavbarContent
+				className='hidden sm:flex basis-1/5 sm:basis-full'
+				justify='end'
+			>
+				<NavbarItem className='hidden sm:flex gap-2'>
+					<ThemeSwitch />
+				</NavbarItem>
+				<NavbarItem className='hidden lg:flex'>{searchInput}</NavbarItem>
+				<NavbarItem className='hidden md:flex'>
+					{status === 'loading' ? (
+						<div className=' w-[75px] h-[75px]'>
+							<Lottie animationData={Loaded} />
+						</div>
+					) : session ? (
+						<div className='flex gap-3  items-center'>
+							<CartModal />
+							<UserDropdown user={session?.user.id} />
+						</div>
+					) : (
+						<Button as={NextLink} href='/auth/login'>
+							Login
+						</Button>
+					)}
+				</NavbarItem>
+			</NavbarContent>
 
-      <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-        <Link isExternal aria-label="Github" href={"#"}>
-          <GithubIcon className="text-default-500" />
-        </Link>
-        <ThemeSwitch />
-        <NavbarMenuToggle />
-      </NavbarContent>
-    </NextUINavbar>
-  );
+			<NavbarContent className='sm:hidden basis-1 pl-4' justify='end'>
+				<Link isExternal aria-label='Github' href={'#'}>
+					<GithubIcon className='text-default-500' />
+				</Link>
+				<ThemeSwitch />
+				<NavbarMenuToggle />
+			</NavbarContent>
+		</NextUINavbar>
+	);
 };
