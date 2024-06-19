4b5f7132c5fe7400aafccfb2f7a5eafc15c7e15a
Pavel Maksimovich <63461675+Fox1N69@users.noreply.github.com>
2024-06-19 20:34:04 +0500

    Изменение аватарки

--- autopart-shop/components/navbar/user-dropdown.tsx
+++ autopart-shop/components/navbar/user-dropdown.tsx
@@ -1,97 +1,97 @@
 import {
-  Avatar,
-  Dropdown,
-  DropdownItem,
-  DropdownMenu,
-  DropdownTrigger,
-  NavbarItem,
-} from "@nextui-org/react";
+	Avatar,
+	Dropdown,
+	DropdownItem,
+	DropdownMenu,
+	DropdownTrigger,
+	NavbarItem
+} from '@nextui-org/react';
 
-import { useRouter, useSearchParams } from "next/navigation";
-import React, { useEffect, useState } from "react";
-import Cookies from "js-cookie";
-import { Switch } from "@nextui-org/switch";
-import { ThemeSwitch } from "../theme-switch";
-import { UserInfo } from "os";
-import { User } from "@/types";
-import axios from "axios";
-import { fetchData } from "next-auth/client/_utils";
-import { signOut, useSession } from "next-auth/react";
+import { useRouter, useSearchParams } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
+import Cookies from 'js-cookie';
+import { Switch } from '@nextui-org/switch';
+import { ThemeSwitch } from '../theme-switch';
+import { UserInfo } from 'os';
+import { User } from '@/types';
+import axios from 'axios';
+import { fetchData } from 'next-auth/client/_utils';
+import { signOut, useSession } from 'next-auth/react';
 
 interface Props {
-  user: string;
+	user: string;
 }
 
 export const UserDropdown: React.FC<Props> = ({ user }) => {
-  const { data: session } = useSession();
-  const [fio, setFio] = useState<string>("");
-  const route = useRouter();
+	const { data: session } = useSession();
+	const [fio, setFio] = useState<string>('');
+	const route = useRouter();
 
-  const handleLogout = async () => {
-    signOut({ callbackUrl: "/auth/login" });
-  };
+	const handleLogout = async () => {
+		signOut({ callbackUrl: '/auth/login' });
+	};
 
-  useEffect(() => {
-    const fetchUserData = async () => {
-      if (!session || !session.user || !session.user.token) {
-        console.log("No user token found in session");
-        return;
-      }
-      try {
-        const response = await axios.get(
-          `http://localhost:4000/v1/account/user/${user}`,
-          {
-            headers: {
-              Authorization: `Bearer ${session.user.token}`,
-            },
-          }
-        );
-        setFio(response.data.fio);
-      } catch (error) {
-        console.error("Error fetching user data:", error);
-      }
-    };
+	useEffect(() => {
+		const fetchUserData = async () => {
+			if (!session || !session.user || !session.user.token) {
+				console.log('No user token found in session');
+				return;
+			}
+			try {
+				const response = await axios.get(
+					`http://localhost:4000/v1/account/user/${user}`,
+					{
+						headers: {
+							Authorization: `Bearer ${session.user.token}`
+						}
+					}
+				);
+				setFio(response.data.fio);
+			} catch (error) {
+				console.error('Error fetching user data:', error);
+			}
+		};
 
-    fetchUserData();
-  }, [user, session]);
+		fetchUserData();
+	}, [user, session]);
 
-  return (
-    <Dropdown>
-      <NavbarItem>
-        <DropdownTrigger>
-          <Avatar
-            as="button"
-            color="secondary"
-            size="md"
-            src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-          />
-        </DropdownTrigger>
-      </NavbarItem>
-      <DropdownMenu
-        aria-label="User menu actions"
-        onAction={(actionKey) => console.log({ actionKey })}
-      >
-        <DropdownItem
-          key="profile"
-          className="flex flex-col justify-start w-full items-start"
-        >
-          <p>{fio}</p>
-        </DropdownItem>
-        <DropdownItem key="settings">Настройки</DropdownItem>
-        <DropdownItem key="system">Система</DropdownItem>
-        <DropdownItem key="configurations">Конфигурация</DropdownItem>
-        <DropdownItem key="help_and_feedback">
-          Помощь и обратная связь
-        </DropdownItem>
-        <DropdownItem
-          key="logout"
-          color="danger"
-          className="text-danger"
-          onClick={handleLogout}
-        >
-          Выйти
-        </DropdownItem>
-      </DropdownMenu>
-    </Dropdown>
-  );
+	return (
+		<Dropdown>
+			<NavbarItem>
+				<DropdownTrigger>
+					<Avatar
+						as='button'
+						color='secondary'
+						size='md'
+						src='/avatar-ilya.jpg'
+					/>
+				</DropdownTrigger>
+			</NavbarItem>
+			<DropdownMenu
+				aria-label='User menu actions'
+				onAction={actionKey => console.log({ actionKey })}
+			>
+				<DropdownItem
+					key='profile'
+					className='flex flex-col justify-start w-full items-start'
+				>
+					<p>{fio}</p>
+				</DropdownItem>
+				<DropdownItem key='settings'>Настройки</DropdownItem>
+				<DropdownItem key='system'>Система</DropdownItem>
+				<DropdownItem key='configurations'>Конфигурация</DropdownItem>
+				<DropdownItem key='help_and_feedback'>
+					Помощь и обратная связь
+				</DropdownItem>
+				<DropdownItem
+					key='logout'
+					color='danger'
+					className='text-danger'
+					onClick={handleLogout}
+				>
+					Выйти
+				</DropdownItem>
+			</DropdownMenu>
+		</Dropdown>
+	);
 };
