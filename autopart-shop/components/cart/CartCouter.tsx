d635aa71fdfa3d828c0a7aa3a11334471a220b3f
Pavel Maksimovich <63461675+Fox1N69@users.noreply.github.com>
2024-06-20 05:21:58 +0500

    Попытк сделать подсчет общей стоимости в корзине в прямом времяни не увенчался успехом

--- autopart-shop/components/cart/CartCouter.tsx
+++ autopart-shop/components/cart/CartCouter.tsx
@@ -1,72 +1,76 @@
-import { useSession } from "next-auth/react";
-import React, { useState, useEffect } from "react";
+import { useSession } from 'next-auth/react';
+import React, { useState, useEffect } from 'react';
 
 interface CartCounterProps {
-  autoPartID: number;
-  initialQuantity: number;
+	autoPartID: number;
+	initialQuantity: number;
+	onQuantityChange: (id: number, count: number) => void;
 }
 
 const CartCounter: React.FC<CartCounterProps> = ({
-  autoPartID,
-  initialQuantity,
+	autoPartID,
+	initialQuantity,
+	onQuantityChange
 }) => {
-  const [count, setCount] = useState<number>(initialQuantity);
-  const { data: session } = useSession();
+	const [count, setCount] = useState<number>(initialQuantity);
+	const { data: session } = useSession();
 
-  const updateQuantityOnServer = async (newQuantity: number) => {
-    try {
-      const response = await fetch(
-        `http://localhost:4000/v1/account/user/${session?.user.id}/update_basket/${autoPartID}`,
-        {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${session?.user.token}`,
-          },
-          body: JSON.stringify({ quantity: newQuantity }),
-        }
-      );
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-    } catch (error) {
-      console.error("Failed to update quantity on server", error);
-    }
-  };
+	const updateQuantityOnServer = async (newQuantity: number) => {
+		try {
+			const response = await fetch(
+				`http://localhost:4000/v1/account/user/${session?.user.id}/update_basket/${autoPartID}`,
+				{
+					method: 'PUT',
+					headers: {
+						'Content-Type': 'application/json',
+						Authorization: `Bearer ${session?.user.token}`
+					},
+					body: JSON.stringify({ quantity: newQuantity })
+				}
+			);
+			if (!response.ok) {
+				throw new Error('Network response was not ok');
+			}
+		} catch (error) {
+			console.error('Failed to update quantity on server', error);
+		}
+	};
 
-  const handleIncrement = () => {
-    const newCount = count + 1;
-    setCount(newCount);
-    updateQuantityOnServer(newCount);
-  };
+	const handleIncrement = () => {
+		const newCount = count + 1;
+		setCount(newCount);
+		updateQuantityOnServer(newCount);
+		onQuantityChange(autoPartID, newCount);
+	};
 
-  const handleDecrement = () => {
-    if (count > 1) {
-      const newCount = count - 1;
-      setCount(newCount);
-      updateQuantityOnServer(newCount);
-    }
-  };
+	const handleDecrement = () => {
+		if (count > 1) {
+			const newCount = count - 1;
+			setCount(newCount);
+			updateQuantityOnServer(newCount);
+			onQuantityChange(autoPartID, newCount);
+		}
+	};
 
-  return (
-    <div className="flex items-center space-x-4">
-      <button
-        onClick={handleDecrement}
-        className="px-2 py-1 bg-gray-200 rounded-md hover:bg-gray-300"
-      >
-        -
-      </button>
-      <div className="w-8 h-8 flex items-center justify-center border border-gray-300 rounded-md">
-        {count}
-      </div>
-      <button
-        onClick={handleIncrement}
-        className="px-2 py-1 bg-gray-200 rounded-md hover:bg-gray-300"
-      >
-        +
-      </button>
-    </div>
-  );
+	return (
+		<div className='flex items-center space-x-4'>
+			<button
+				onClick={handleDecrement}
+				className='px-2 py-1 bg-gray-200 rounded-md hover:bg-gray-300'
+			>
+				-
+			</button>
+			<div className='w-8 h-8 flex items-center justify-center border border-gray-300 rounded-md'>
+				{count}
+			</div>
+			<button
+				onClick={handleIncrement}
+				className='px-2 py-1 bg-gray-200 rounded-md hover:bg-gray-300'
+			>
+				+
+			</button>
+		</div>
+	);
 };
 
 export default CartCounter;
