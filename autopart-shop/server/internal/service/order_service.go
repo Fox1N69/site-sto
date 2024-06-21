3dc9f941739ca98b26bbcfb7a976d32a9adef71b
Pavel Maksimovich <63461675+Fox1N69@users.noreply.github.com>
2024-06-22 00:31:26 +0500

    реализовал сервис для order

--- autopart-shop/server/internal/service/order_service.go
+++ autopart-shop/server/internal/service/order_service.go
@@ -1,6 +1,14 @@
 package service
 
-import "shop-server/internal/repo"
+import (
+	"context"
+	"errors"
+	"fmt"
+	"shop-server/internal/model"
+	"shop-server/internal/repo"
+
+	"gorm.io/gorm"
+)
 
 type OrderService interface {
 }
@@ -11,4 +19,33 @@ type orderService struct {
 
 func NewOrderService(orderRepo repo.OrderRepo) OrderService {
 	return orderService{repo: orderRepo}
+}
+
+func (s *orderService) CreateOrder(order *model.Order) error {
+	if err := s.repo.CreateOrder(order); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *orderService) GetAllOrders(ctx context.Context) ([]model.Order, error) {
+	orders, err := s.repo.GetAllOrders()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get all orders %w", err)
+	}
+
+	return orders, nil
+}
+
+func (s *orderService) UpdateOrder(ctx context.Context, id uint, orderData map[string]interface{}) error {
+
+	if err := s.repo.UpdateOrder(id, orderData); err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return fmt.Errorf("order not found: %w", err)
+		}
+		return fmt.Errorf("failed to update order: %w", err)
+	}
+
+	return nil
 }
