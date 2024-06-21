701c2362c103db930deb63b4b67af565222e41b4
Pavel Maksimovich <63461675+Fox1N69@users.noreply.github.com>
2024-06-21 21:04:22 +0500

    Работа на экраном оплаты

--- autopart-shop/app/checkout/page.tsx
+++ autopart-shop/app/checkout/page.tsx
@@ -1,23 +1,28 @@
 'use client';
 import { useCartStore } from '@/store/cartStore';
-import { Product } from '@/types';
-import { useState } from 'react';
+import { Input } from '@nextui-org/input';
+import { DevIcon } from '../../components/icons/sidebar/dev-icon';
+import { Card, CardBody, CardHeader } from '@nextui-org/react';
 
 export default function CheckOutPage() {
-	const [products, setProducts] = useState<Product[]>([]);
 	const { itemsInCart, totalPrice } = useCartStore();
 
 	return (
 		<section>
-			<ul>
+			<div className='flex gap-5'>
 				{Object.values(itemsInCart).map(item => (
-					<li key={item.id}>
+					<>
 						<p>{item.name}</p>
 						<p>{item.price}</p>
-						<p>{item.quantity}</p>
-					</li>
+					</>
 				))}
-			</ul>
+			</div>
+
+			<div>
+				<form action=''>
+					<Input label='test' />
+				</form>
+			</div>
 		</section>
 	);
 }
