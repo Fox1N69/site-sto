e8906b9e576f9216fac148ae2ea19b51115eeb5e
Pavel Maksimovich <63461675+Fox1N69@users.noreply.github.com>
2024-06-22 00:17:26 +0500

    Реализовл репозиторий для order

--- autopart-shop/server/internal/repo/order_repo.go
+++ autopart-shop/server/internal/repo/order_repo.go
@@ -1,8 +1,16 @@
 package repo
 
-import "gorm.io/gorm"
+import (
+	"shop-server/internal/model"
+
+	"gorm.io/gorm"
+)
 
 type OrderRepo interface {
+	CreateOrder(order *model.Order) error
+	GetAllOrders() ([]model.Order, error)
+	UpdateOrder(id uint, orderData map[string]interface{}) error
+	DeleteOrder(id uint) error
 }
 
 type orderRepo struct {
@@ -10,5 +18,68 @@ type orderRepo struct {
 }
 
 func NewOrderRepo(db *gorm.DB) OrderRepo {
-	return orderRepo{db: db}
+	return &orderRepo{db: db}
+}
+
+func (r *orderRepo) CreateOrder(order *model.Order) error {
+	return r.db.Create(order).Error
+}
+
+func (r *orderRepo) GetAllOrders() ([]model.Order, error) {
+	var order []model.Order
+
+	result := r.db.Preload("Users").Find(&order)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return order, nil
+}
+
+func (r *orderRepo) UpdateOrder(id uint, orderData map[string]interface{}) error {
+	var order model.Order
+	result := r.db.Preload("Users").Where("id = ?", id).Find(&order)
+	if result.Error != nil {
+		return result.Error
+	}
+
+	userData := order.User
+	userEmail := userData.Email
+	userPhone := userData.PhoneNumber
+	userAddress := userData.DeliveryAddress
+
+	for key, value := range orderData {
+		switch key {
+		case "status":
+			order.Status = value.(string)
+		case "email":
+			userEmail = value.(string)
+		case "phone":
+			userPhone = value.(string)
+		case "address":
+			userAddress = value.(string)
+		}
+	}
+
+	r.db.Model(&userData).Updates(map[string]interface{}{
+		"email":   userEmail,
+		"phone":   userPhone,
+		"address": userAddress,
+	})
+
+	result = r.db.Save(&order)
+	if result.Error != nil {
+		return result.Error
+	}
+
+	return nil
+}
+
+func (r *orderRepo) DeleteOrder(id uint) error {
+	var order model.Order
+	result := r.db.Where("id = ?", id).First(&order)
+	if result.Error != nil {
+		return result.Error
+	}
+	return r.db.Delete(&order).Error
 }
