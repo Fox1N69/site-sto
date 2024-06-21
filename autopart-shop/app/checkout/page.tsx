d83a6e50b4cab798ce0ed9bb2f0039ed2ce4ed76
Pavel Maksimovich <63461675+Fox1N69@users.noreply.github.com>
2024-06-22 02:22:10 +0500

    сделал форму создания заявки

--- autopart-shop/app/checkout/page.tsx
+++ autopart-shop/app/checkout/page.tsx
@@ -1,14 +1,14 @@
 'use client';
 import { useCartStore } from '@/store/cartStore';
-import { Input } from '@nextui-org/input';
+import { Input, Textarea } from '@nextui-org/input';
 import { DevIcon } from '../../components/icons/sidebar/dev-icon';
-import { Card, CardBody, CardHeader } from '@nextui-org/react';
+import { Button, Card, CardBody, CardHeader } from '@nextui-org/react';
 
 export default function CheckOutPage() {
 	const { itemsInCart, totalPrice } = useCartStore();
 
 	return (
-		<section>
+		<section className=''>
 			<div className='flex gap-5'>
 				{Object.values(itemsInCart).map(item => (
 					<>
@@ -18,10 +18,41 @@ export default function CheckOutPage() {
 				))}
 			</div>
 
-			<div>
-				<form action=''>
-					<Input label='test' />
-				</form>
+			<div className='flex flex-col justify-center items-center mt-20'>
+				<Card className='w-[50%] mt-5 py-4 pb-5'>
+					<CardHeader className='flex justify-center'>
+						<h1 className=''>Оформление заявки</h1>
+					</CardHeader>
+					<CardBody className='flex flex-col justify-center items-center'>
+						<form action='submit' className='w-[90%] flex flex-col gap-5'>
+							<Input
+								label='Город'
+								labelPlacement='outside'
+								variant='flat'
+								color='primary'
+							/>
+							<Input
+								label='Адресс доставки'
+								labelPlacement='outside'
+								variant='flat'
+								color='primary'
+							/>
+							<Textarea
+								type='text'
+								label='Комментарий'
+								labelPlacement='outside'
+								variant='flat'
+								color='primary'
+							/>
+						</form>
+						<div className='w-[90%] mt-20 flex flex-col gap-5'>
+							<p>Общая стоимость заказа: {totalPrice}</p>
+							<Button variant='ghost' color='primary' className='w-full'>
+								Оформить
+							</Button>
+						</div>
+					</CardBody>
+				</Card>
 			</div>
 		</section>
 	);
