579c1a7439e9891c8b1b7233dcc91f4bcb9ec3e0
Pavel Maksimovich <63461675+Fox1N69@users.noreply.github.com>
2024-06-23 14:10:49 +0500

    Страницы товара

--- autopart-shop/components/autopart/content.tsx
+++ autopart-shop/components/autopart/content.tsx
@@ -1,35 +1,65 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import { AutoPart } from '@/types';
 import axios from 'axios';
 import AutoPartCarousel from '@/components/autopart/AutopartCorusel/corusel';
 import { useFetchPartById } from '@/hooks/fetching';
 import { DevIcon } from '../icons/sidebar/dev-icon';
+import { Button } from '@nextui-org/button';
+import { Card, CardBody, CardFooter, CardHeader } from '@nextui-org/react';
 
 export const AutoPartContent = ({ id }: { id: number }) => {
-	const { part, error } = useFetchPartById(id);
+	const { part, error, isLoading } = useFetchPartById(id);
+	const [isBtnActive, setBtnActive] = useState<boolean>(false);
 
 	return (
-		<div className=''>
-			<div className='flex gap-14'>
-				<div className='w-[300px] h-[250px] border-1'>
-					<img src={part?.img} alt='' />
-				</div>
-				<div className='flex flex-col gap-14'>
-					<div className='flex flex-col gap-5'>
-						<h2 className='font-bold text-4xl'>{part?.name}</h2>
-						<p>{part?.model_name}</p>
+		<>
+			{isLoading ? (
+				<p>loading...</p>
+			) : (
+				<div className='flex justify-between items-center'>
+					<div className='flex gap-10'>
+						<div className='min-w-[325px] h-[250px] border-1'>{part?.img}</div>
+						<div className='flex flex-col gap-14'>
+							<div className='flex flex-col gap-5'>
+								<h2 className='font-bold text-4xl'>{part?.name}</h2>
+								<p>{part?.model_name}</p>
+							</div>
+							<div className='flex flex-col'>
+								{part?.auto_part_info?.map(info => (
+									<div className='flex items-center gap-5'>
+										<h3 className=' font-medium text-lg'>{info.title}:</h3>
+										<p>{info.description}</p>
+									</div>
+								))}
+							</div>
+						</div>
 					</div>
 					<div>
-						{part?.auto_part_info?.map(info => (
-							<div className='flex '>
-								<h3>{info.title}:</h3>
-								<p>{info.description}</p>
-							</div>
-						))}
+						<Card className='w-[225px] h-[160px] pb-3 '>
+							<CardHeader className='flex justify-between'>
+								<>
+									<p className='font-semibold text-2xl'>Цена: </p>
+									<div className='border-1 rounded-lg p-2'>
+										<h3 className='font-semibold text-2xl'>{part?.price}р</h3>
+									</div>
+								</>
+							</CardHeader>
+							<CardBody>
+								{isBtnActive ? (
+									<Button className='w-full' variant='flat' color='primary'>
+										Перейти в корзину
+									</Button>
+								) : (
+									<Button className='w-full' variant='shadow' color='primary'>
+										В корзину
+									</Button>
+								)}
+							</CardBody>
+						</Card>
 					</div>
 				</div>
-			</div>
-		</div>
+			)}
+		</>
 	);
 };
